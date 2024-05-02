a6699f0cf442070777c77213ba1f7ada8928bd50
Kavindu Prabhashitha <kavindu.prabhashitha@gmail.com>
2024-05-02 15:40:53 +0530

    import data module added

--- src/app/components/import-data/import-data.component.ts
+++ src/app/components/import-data/import-data.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, Component } from '@angular/core';
 
 @Component({
   selector: 'app-import-data',
   templateUrl: './import-data.component.html',
   styleUrl: './import-data.component.css'
 })
-export class ImportDataComponent {
+export class ImportDataComponent implements AfterContentChecked{
 
+ isLoading=false;
+ countryName="";
+
+ importData(){
+  this.isLoading = true
+  setTimeout(()=>{
+    this.isLoading=false
+  },5000);
+
+ }
+
+ ngAfterContentChecked(): void {
+  console.log(this.countryName);
+}
+ 
 }
